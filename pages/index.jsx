a65e8e0f5841298f2b13eeebb5a861fdde6ead5c
pedrotchang <pedrochang91@gmail.com>
2022-08-23 15:03:36 -0700

    initial commit

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,6 +11,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <div className="font-bold text-9xl">Hello</div>
+      
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
