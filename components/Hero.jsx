03074903e5ac51d7637005d624b44a1283c201dc
pedrotchang <pedrochang91@gmail.com>
2022-08-25 10:44:52 -0700

    finally, completes hero 100%

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -30,11 +30,12 @@ const Hero = ({ heading, message }) => {
   return (
      <div className='min-h-screen flex flex-col items-center justify-center'>
       <div className='absolute w-full h-screen bg-fixed bg-center bg-cover custom-img'/>
+       <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/60 z-[2]" />
         <div className={`flex-grow-0 pt-10 transition-opacity duration-1000`}>
       </div>
       <div className='p-12 font-bold z-10 text-white drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)] text-center flex-1 flex items-center justify-center flex-col'>
-        <h1 className='mb-6 text-4xl xl:text-5xl'>Give Water Give Life</h1>
-        <h2 className='pb-6 mb-2 text-2xl xl:text-3xl tracking-tight'>
+        <h1 className='mb-6 text-4xl xl:text-6xl'>Give Water Give Life</h1>
+        <h2 className='pb-6 mb-2 text-left max-w-4xl text-2xl xl:text-3xl tracking-tight'>
           <span>{message}</span>
         </h2>
        <button className="px-7 py-2 text-3xl border rounded-md">Donate</button>
